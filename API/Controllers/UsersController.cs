da646c416dbfafeeecbdbaca48ac6fbf76bda58a
Gianni Perez <gianniperez1028@gmail.com>
2022-07-09 18:00:57 -0400

    Add project files.

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUserById(int id)
+        {
+            return Ok(await _context.Users.FindAsync(id));
+        }
+
+    }
+}
---
+++ API/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        public bool? IsActive { get; set; }
+        public DateTime? DateOfBirth { get; set; } // DateOnly datatype cannot be used as it isn't fully supported by entity framework
+        public byte[] PaswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
